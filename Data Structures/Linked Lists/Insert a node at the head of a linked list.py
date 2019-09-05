151304ddcda4c81fa3baec788b1c46b5483d640a
TapasDash <tapasdash017@gmail.com>
2019-09-05 19:52:45 +0530

    Create Insert a node at the head of a linked list.py

---
+++ Data Structures/Linked Lists/Insert a node at the head of a linked list.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtHead function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtHead(head, data):
+    node = SinglyLinkedListNode(data)
+    if head is not None:
+        node.next = head
+        head = node
+    else:
+        head = node
+    return(head)
+    
+
+if __name__ == '__main__':
